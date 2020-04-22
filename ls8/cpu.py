938df61307986961423b6893e70b957f541db8b8
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-04-22 00:39:09 -0700

    implemented load() and implemented multiply instruction

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -26,32 +26,56 @@ class CPU:
     def ram_write(self, mdr, mar):
         self.ram[mar] = mdr
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            address = 0
+            # open the file
+            with open(sys.argv[1]) as f:
+                # read every line
+                for line in f:
+                    # parse out comments
+                    comment_split = line.strip().split("#")
+                    # Cast number string to int
+                    value = comment_split[0].strip()
+                    # ignore blank lines
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    # populate memory array
+                    self.ram[address] = instruction
+                    address += 1
+
+        except:
+            print("cant find file")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -79,9 +103,11 @@ class CPU:
     def run(self):
         """Run the CPU."""
         while True:
+            # self.trace()
             opcode = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+
             if opcode == LDI:
                 # set the value of a register to an integer
                 self.reg[operand_a] = operand_b
